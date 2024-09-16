f3735532c2c681f4d9e67a06270f7db1b0d5e220
smasr07 <a.sobirjonov@newuu.uz>
2024-09-16 20:12:38 +0500

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int a, b;
-    cin >> a >> b;
-    if (a == 1 ) { cout << "Bus " << endl; }
-    else if ( b == 1 ) { cout << " Walk " << endl; }
+   int a, b, c;
+   cin >> a >> b ;
+   if (a == 1 ) { cout << "Bus " << endl; }
+   else if ( a == 0 && b == 1 ) { cout << " Walk " << endl; }
+   return 0;
 }
