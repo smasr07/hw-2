d864ae66f5d074c30769e2ee0511ae4036c10d76
smasr07 <a.sobirjonov@newuu.uz>
2024-09-16 16:07:32 +0500

    Create problem2.cpp

---
+++ problem2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if (a == 1 ) { cout << "Bus " << endl; }
+    else if ( b == 1 ) { cout << " Walk " << endl; }
+}
